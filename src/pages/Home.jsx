cb0d49fcbe521a5fe06373fd35dcb04071d3376a
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-06-19 16:01:28 +0200

    Added logic to Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,23 @@
 //Home page
-
+import EventCards from '../components/EventCards';
+import { useEvent } from '../context/EventContext';
 
 const Home = () => {
 
-    return (
-<div>
+    const { events, dataIsLoaded, error } = useEvent();
 
+    const sortedEvents = [...events].sort(
+        (a, b) => new Date(b.date) - new Date(a.date)
+     );
+
+    if (!dataIsLoaded) return <p>Loading events...</p>;
+    if (error) return <p>Something went wrong: {error} </p>;
+
+    return (
+ <div className="flex flex-wrap gap-6 p-4 justify-start">
+            {sortedEvents.map((event) => (
+                <EventCards key={event.date} {...event} />
+            ))}
 </div>
     );
 };
