0e6b66b66dfdd2862262a8a227b4b9611d2d4a92
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-06-20 13:40:24 +0200

    adjusted navbar postion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,17 +5,17 @@ import { Link } from "react-router";
 
 const Navbar = () => {
     return (
-        <header className="fixed bg-transparent border-none w-screen h-20 z-20 mx-auto" >
-            <nav className="max-w-screen flex flex-wrap items-center justify-around mx-auto p-4">
+        <header className="absolute top-0 bg-transparent w-full h-20 z-50 mx-auto" >
+            <nav className="max-w-screen flex items-end justify-around mx-auto p-4">
             <div>
-            <h1 className=" text-[#150046] text-[2.5rem]">EventBeat</h1>
+            <p className=" text-[#150046] text-3xl font-bold tracking-tight">EventBeat</p>
             </div>
-            <div className="flex gap-5">
-            <Link to='/registraion' >
+            <div className="flex items-end gap-5">
+            <Link to='/registration' >
 			<button className='mt-4 border-2 border-gray-600 p-3 rounded-full'>Sign up</button>
 			</Link>
              <Link to='/login'
-                className="flex hover:bg-blue-950 transition duration-200 align-bottom"
+                className=" hover:text-white hover:underline transition duration-200 pb-2"
             > Already registerd? Log in here
             </Link>
             </div>
