19c635f6b25cce8eb1132cd4b5c46dcd07f321a7
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-06-23 19:45:14 +0200

    adjusted but not finished buttons on homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     const { isAuthenticated } = useAuth();
 
     return (
-        <div className="relative bg-center bg-[url(./img/pexels-annamw-1047442.jpg)] h-screen bg-no-repeat bg-cover">
+        <div className="relative bg-center bg-[url(./img/pexels-annamw-1047442.jpg)] h-screen bg-no-repeat bg-cover ">
             <div className="relative z-40 text-center pt-50">
                 <h1  className="text-5xl md:text-6xl text-white font-bold"> Welcome to <br></br>
                     <p className="text-5xl md:text-8xl text-white font-bold">EventBeat</p></h1>
@@ -14,19 +14,19 @@ const Hero = () => {
 
             <div> { isAuthenticated? (
                  <Link to='/createevent' >
-			<button className='mt-4 border-2 border-white p-3 text-pink-500 font-semibold rounded-full hover:bg-white transition duration-200 '>Create Event</button>
+			<button className='mt-4 border-2 border-white p-3 bg-white text-pink-500 font-bold rounded-full text-xl hover:cursor-pointer hover:border-pink-500 hover:bg-pink-500  hover:text-white hover:shadow-xl transition duration-200 mx-5 '>Create Event</button>
 			</Link> ) : (
                 <>
             <Link to='/registration' >
-			<button className='mt-4 border-2 border-white p-3 text-pink-500 font-semibold rounded-full hover:bg-white transition duration-200 '>Sign up</button>
+			<button className='mt-4 border-2 border-white p-3 bg-white text-pink-500 font-bold rounded-full text-xl hover:cursor-pointer hover:border-pink-500 hover:bg-pink-500  hover:text-white hover:shadow-xl transition duration-200 mx-5 '>Sign up</button>
 			</Link>
             <Link to='/createevent' >
-			<button className='mt-4 border-2 border-white p-3 text-pink-500 font-semibold rounded-full hover:bg-white transition duration-200 '>Create Event</button>
+			<button className='mt-4 border-2 border-white p-3 bg-white text-pink-500 font-bold rounded-full text-xl hover:cursor-pointer hover:border-pink-500 hover:bg-pink-500  hover:text-white hover:shadow-xl transition duration-200 mx-5 '>Create Event</button>
 			</Link>
             </>
             )}
             </div>
-            </div>
+         </div>
         </div>
     );
 };
