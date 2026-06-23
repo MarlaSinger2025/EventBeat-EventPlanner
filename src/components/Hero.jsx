ca936aeea0f27b022c4152943fadd2fd26115e63
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-06-23 15:40:54 +0200

    started with editing hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,9 @@ import { Link } from "react-router";
 const Hero = () => {
     return (
         <div className="relative bg-center bg-[url(./img/pexels-annamw-1047442.jpg)] h-screen bg-no-repeat bg-cover">
-            <div className="relative z-40 text-center pt-[150px]">
-                <h1  className="text-5xl md:text-6xl text-white font-bold"> Welcome to EventBeat</h1>
+            <div className="relative z-40 text-center pt-50">
+                <h1  className="text-5xl md:text-6xl text-white font-bold"> Welcome to <br></br>
+                    <p className="text-5xl md:text-8xl text-white font-bold">EventBeat</p></h1>
                 <h2 className="text-3xl md:text-4xl  text-white font-semibold"> You can look for cool events or post an event yourself</h2>
             <Link to='/registration' >
 			<button className='mt-4 border-2 border-white p-3 text-pink-500 font-semibold rounded-full hover:bg-white transition duration-200 '>Sign up</button>
